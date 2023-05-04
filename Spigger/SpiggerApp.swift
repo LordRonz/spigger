202d179401be7e612d73d1c767b877e63315326f
LordRonz <aarontanhar2000@gmail.com>
2023-05-04 14:21:30 +0700

    Initial Commit

---
+++ Spigger/SpiggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpiggerApp.swift
+//  Spigger
+//
+//  Created by Aaron Christopher Tanhar on 04/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SpiggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
